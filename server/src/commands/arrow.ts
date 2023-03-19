d10ec3be01e22b4973b9de2b1fe190cb91684b44
Mykal Machon <mykal@tinybox.dev>
2023-03-18 22:56:47 -0700

    feat(server): saving notes to db

--- server/src/commands/arrow.ts
+++ server/src/commands/arrow.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import { SlashCommandBuilder } from "discord.js";
 import { Command } from "../interfaces"
 
@@ -9,10 +10,27 @@ const pingCommand: Command = {
     .addStringOption((option) => option.setName('title').setDescription('message title').setRequired(true))
     .addStringOption((option) => option.setName('body').setDescription('message body').setRequired(true)),
   execute: async (interaction) => {
-    const title = interaction.options.getString('title');
-    const body = interaction.options.getString('body');
+    try {
+      const title = interaction.options.getString('title') || '';
+      const body = interaction.options.getString('body');
 
-    interaction.reply(`sending "**${title}**: ${body}"...`)
+      const prisma = new PrismaClient();
+      const newNote = await prisma.note.create({
+        data: {
+          title: title,
+          body: body,
+          author: 'Cupid'
+        }
+      });
+      console.log(`[INFO] created new note with title ${newNote.title}`);
+
+      await interaction.reply(`sending "**${title}**: ${body}"...`)
+      await prisma.$disconnect();
+    } catch (err) {
+      interaction.followUp('something went wrong when sending your arrow...');
+      console.log('[ERROR] something went wrong')
+      console.log(err);
+    }
   }
 }
 
