86b65c44f11b3b5570fe9283ea04383641231c5d
Mykal Machon <mykal@tinybox.dev>
2023-04-01 14:18:15 -0700

    feat(server): adding versioning and single arrow fetch

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,17 @@ const getArrows = async () => {
   return arrows;
 }
 
+const getArrow = async (id: string) => {
+  const prisma = new PrismaClient()
+  const arrow = await prisma.note.findUnique({
+    where: {
+      id: id
+    }
+  });
+  await prisma.$disconnect()
+  return arrow;
+}
+
 // setup express
 const app: Application = express()
 const port = process.env.PORT || 3000;
@@ -29,9 +40,18 @@ app.get('/meta/ping', (req: Request, res: Response) => {
 })
 
 // requires authentication
-app.get('/arrows', isAuthenticated, async (req: Request, res: Response) => {
+app.get('/v1/arrows', isAuthenticated, async (req: Request, res: Response) => {
   const arrows = await getArrows();
   return res.json(arrows);
+})
+
+app.get('/v1/arrows/:id', isAuthenticated, async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const arrow = await getArrow(id);
+  if (!arrow) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+  return res.json(arrow);
 })
 
 app.listen(port, function () {
