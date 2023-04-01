f0cf73598e352d064ff4c6b92a2c1281dc43b662
Mykal Machon <mykal@tinybox.dev>
2023-04-01 13:16:47 -0700

    feat: moving from fastify to express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,67 +1,32 @@
 import * as dotenv from 'dotenv'
+import express, { Application, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-import BearerAuthPlug from '@fastify/bearer-auth';
 
 // setup environment variables
 dotenv.config()
 
-const server: FastifyInstance = Fastify({});
-const apiKeys = new Set([process.env.API_KEY]);
-
-// @ts-ignore: should fix apiKeys and process.env stuff above
-server.register(BearerAuthPlug, { keys: apiKeys, addHook: true });
-
-server.route({
-  method: 'GET',
-  url: '/ping',
-  handler: async (request, reply) => {
-    return { pong: 'it worked!' }
-  }
-})
-
-
-server.route({
-  method: 'GET',
-  url: '/arrows',
-  handler: async (request, reply) => {
-    const prisma = new PrismaClient()
-    const arrows = await prisma.note.findMany();
-    await prisma.$disconnect()
-    return arrows;
-  }
-})
-
-const start = async () => {
-  try {
-    await server.listen({ port: parseInt(process.env.PORT || '3000') || 3000 })
-
-    const address = server.server.address()?.toString() || 'localhost'
-    const port = process.env.PORT || '3000'
-
-    console.log(`[INFO] server started at http://localhost:${port}`)
-
-  } catch (err) {
-    server.log.error(err)
-    process.exit(1)
-  }
+// setup utility functions
+const getArrows = async () => {
+  const prisma = new PrismaClient()
+  const arrows = await prisma.note.findMany();
+  await prisma.$disconnect()
+  return arrows;
 }
 
-// start the server
-start()
+// setup express
+const app: Application = express()
+const port = process.env.PORT || 3000;
 
-  // const opts: RouteShorthandOptions = {
-  //   schema: {
-  //     response: {
-  //       200: {
-  //         type: 'object',
-  //         properties: {
-  //           pong: {
-  //             type: 'string'
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+app.get('/meta/ping', (req: Request, res: Response) => {
+  return res.json({ message: 'pong' })
+})
+
+app.get('/arrows', async (req: Request, res: Response) => {
+  const arrows = await getArrows();
+  return res.json(arrows);
+})
+
+app.listen(port, function () {
+  console.log(`App is listening on port http://localhost:${port} !`)
+})
